fe71352e4379ce1207f974c2a1cd4da1e8ef66e9
yoonhero <yoonhero06@naver.com>
2022-05-05 10:39:48 +0900

    constant

---
+++ chain.py
@@ -0,0 +1,39 @@
+# from constant import Constant as C
+import constant
+
+
+class Blockchain:
+    def __init__(self):
+        # C.Pi = 3.14
+        # print(C.Pi)
+
+        # C.Pi = 3.141592
+        # print(C.Pi)
+
+        # print(C.__dict__)
+        print("")
+    # persistBlock
+    # restore block
+    # find block
+    # create block
+    # add block
+    # return Blocks
+    # persistBlockchain
+    # Blockchain - return info blockchain
+    # recalculate Difficulty
+    # get Difficulty
+
+
+if __name__ == "__main__":
+    chain = Blockchain()
+
+
+constant.PI = 3.14
+constant.MAX = 1000
+print(constant.PI)
+print(constant.MAX)
+
+constant.PI = 3.141592
+constant.MAX = 2000
+print(constant.PI)
+print(constant.MAX)
---
+++ constant.py
@@ -0,0 +1,15 @@
+import sys
+
+
+class _constant:
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise Exception("Don't allocate value on Constant")
+        self.__dict__[name] = value
+
+    def __delattr__(self, name):
+        if name in self.__dict__:
+            raise Exception("Don't Delete Value")
+
+
+sys.modules[__name__] = _constant()
